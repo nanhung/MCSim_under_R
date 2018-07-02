83ec4f2416ef2eebf1c21ddf63eb8e526cd729dc
nanhung <nhsieh@cvm.tamu.edu>
2018-07-02 08:49:32 -0500

    revise R_script

--- R_script.R
+++ R_script.R
@@ -2,12 +2,14 @@
 Sys.getenv("PATH")
 
 # You have two options to use GNU compiler:
-# If you have installed MinGW you can use (I use MinGW in this case)
+# If you have installed MinGW in your PC you can use
 Sys.setenv(PATH = paste("c:\\MinGW\\bin", Sys.getenv("PATH"), sep=";"))
 
-# If you have installed Rtools you can use
-Sys.setenv(PATH = paste("c:\\Rtools\\bin", Sys.getenv("PATH"), sep=";"))
-
+# Otherwise, if you have installed Rtools, you can assign the bin location manually,
+# Sys.setenv(PATH = paste("c:\\Rtools\\bin", Sys.getenv("PATH"), sep=";"))
+# Sys.setenv(PATH = paste("c:\\Rtools\\mingw_32/bin", Sys.getenv("PATH"), sep=";"))
+Sys.setenv(PATH = paste("c:\\Rtools\\mingw_64/bin", Sys.getenv("PATH"), sep=";"))
+# Sys.setenv(BINPREF = "c:\\Rtools\\mingw_64/bin/")
 
 # Check the GNU compiler 
 Sys.which("gcc")
@@ -37,8 +39,10 @@ out <- read.delim("sim.out", skip = 2)
 # Print
 head (out)
 
-# Plot
+
+# Plot 
+out <- out[2:13,] # omit 0
 par(mfrow = c(2,2))
-plot(out[,1], out.3[,2], log="x",type="b", xlab = "time", ylab = "", main = "y1")
-plot(out[,1], out.3[,3], log="x",type="b", xlab = "time", ylab = "", main = "y2")
-plot(out[,1], out.3[,4], log="x",type="b", xlab = "time", ylab = "", main = "y3")
+plot(out[,1], out[,2], type="b", log = "x", xlab = "time", ylab = "", main = "y1")
+plot(out[,1], out[,3], type="b", log = "x", xlab = "time", ylab = "", main = "y2")
+plot(out[,1], out[,4], type="b", log = "x", xlab = "time", ylab = "", main = "y3")
