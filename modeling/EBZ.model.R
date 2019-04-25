068dffa43726aa94bd5d2aa0c1b63bc7e81f1035
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-04-25 03:46:22 +0000

    Add practice files

---
+++ modeling/EBZ.model.R
@@ -0,0 +1,88 @@
+## Description ####
+# EBZ.model.R 
+#
+# Pratice file (Translate from Berkely Madonna to GNU MCSim) 
+# A PBPK model for Ethylbenzene toxicokinetics in male rats.
+#
+# Units:
+# - time in h
+# - volumes in L
+# - massess in moles
+# - concentration in moles/L
+
+## States ####
+States = {
+
+};
+
+##  Outputs ####
+Outputs = {
+};
+
+## Inputs ####
+Inputs = {  };  # Concentration (inhaled)
+
+## Parameters ####
+# Exposure parameter
+
+# Physiologocal parameter
+
+# substance-specific parameter
+# molecular weight
+
+# blood/air and tissue/blood partition coefficients
+
+# metabolic parameters
+
+# Scaling parameter
+# tissue flows, L/hr
+
+# tissue volumes, L
+
+# the Michaelis–Menten kinetics
+
+# conversion factor
+
+
+## Initialize ####
+Initialize {
+  # body weight scaling 
+
+  # conversion factor
+
+  # tissue flows, L/hr
+
+  # tissue volumes, L
+
+  # the Michaelis–Menten kinetics
+
+}
+
+## Dynamics ####
+Dynamics {
+  # exposure concentration converted to mol/L
+
+  # calculated concentrations of ethylbenzene
+
+  # intake rate
+
+  # metabolism
+
+  # ethylbenzene uptake and metabolism
+
+  # amount of ethylbenzene metabolize
+
+  # mass Balance
+
+}
+
+## CalcOutputs ####
+CalcOutputs {
+  
+  # concentration
+
+  # mass Balance
+
+}
+
+End.
