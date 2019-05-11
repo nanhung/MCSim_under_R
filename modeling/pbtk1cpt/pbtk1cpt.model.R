97054ea5a0e7663378f480b632fd522fbee48dea
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-05-11 16:04:47 -0500

    yodate pbtk1cpt model files

--- modeling/pbtk1cpt/pbtk1cpt.model.R
+++ modeling/pbtk1cpt/pbtk1cpt.model.R
@@ -8,22 +8,11 @@
 # - time in hours
 # - volumes in liters
 #
-# Nan-Hung Hsieh - May 2017
+# Nan-Hung Hsieh - May 2019
 
-States  = {
-  A_rest,  # Quantity in central compartment (mg)
-  A_elim,  # Quantity of elimination (mg)
-}; 
-
-Outputs = {
-  Ccompartment,          # Concentration in central compartment (uM)
-  Celim,
-  A_total};        # Total quantity for mass balance (mg)
-
-Inputs = {Oral_input};     # Drug input in micromoles
-
-
-MW; # molecular weight (g/mole)
+States  = { A_rest };  # Quantity in central compartment (mg)
+Outputs = { Ccompartment };  # Concentration in central compartment (uM)
+Inputs = { Oral_input }; # Drug input (uM)
 
 # Oral input modeling
 Absdose;
@@ -31,28 +20,17 @@ IngDose    = 1.0; # ingested input (mg)
 Fgutabs    = 1.0; #
 Period     = 0.0; # period of the exposure/no exposure cycle (h)
 Tlag       = 0.0; # Absorption lagtime (h)
-kgutabs        = 0.1; # Intestinal absortion rate (/h)
+kgutabs    = 0.1; # Intestinal absortion rate (/h)
 Oral_input = PerExp (Absdose, Period, Tlag, kgutabs);
-
-# Volumes (L)
-Vdist  = 1; 
-
-# Transfer rate constants between compartments (/h)
+# Molecular weight (g/mole)
+MW;
+# Distribution volumes (L)
+Vdist  = 1;
+# Elimination rate constants (/h)
 kelim = 1;
 
-Initialize{
-  Absdose = IngDose * Fgutabs;
-}
-
-Dynamics {
-  dt (A_rest)  = kgutabs * Oral_input - kelim * A_rest;
-  dt (A_elim)  = kelim * A_rest; 
-}
-
-CalcOutputs {
-  Ccompartment  = A_rest  / Vdist / MW * 1000;
-  Celim  = A_elim * 1000 / MW;
-  A_total = A_rest + A_elim;
-}
+Initialize{ Absdose = IngDose * Fgutabs; }
+Dynamics { dt (A_rest) = kgutabs * Oral_input - kelim * A_rest; }
+CalcOutputs { Ccompartment  = A_rest  / Vdist / MW * 1000; }
 
 End.
