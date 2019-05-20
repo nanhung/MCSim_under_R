7b82b60b688a3ebb369190827502a80201d8af26
nanhung <nhsieh@cvm.tamu.edu>
2019-05-20 14:27:26 -0500

    modeling files

---
+++ modeling/perc/perc_mcmc.in.R
@@ -0,0 +1,228 @@
+# ------------------------------------------
+# perc_mcmc.in.R (sourced from mcsim-6.1.0)
+
+Integrate (Lsodes, 1e-6, 1e-6, 1);
+
+MCMC ("MCMC.default.out","",  # name of output and restart file
+      "",                     # name of data file
+      4000,0,                 # iterations, print predictions flag,
+      1,4000,                 # printing frequency, iters to print
+      244176216.272593);              # random seed (default )
+
+Level {
+
+  Distrib (sc_Vmax,      TruncLogNormal, 0.042, 10,  4.20E-4, 4.20000);
+  Distrib (Km,           TruncLogNormal, 14.9,  10,  14.9E-2, 14.9E+2);
+
+  Likelihood (C_exh_ug,  LogNormal, Prediction(C_exh_ug), 1.1);
+  Likelihood (C_ven,     LogNormal, Prediction(C_ven),    1.1);
+
+  Level { # all subjects grouped
+
+  Experiment { # 1: Subject A 72 ppm
+
+      LeanBodyWt = 62;
+      Pct_M_fat = 0.114;
+      Flow_pul = 7.6;
+
+      InhMag = 72;    # ppm
+      Period = 1e10;  # Only one dose
+      Exposure = 240; # 4 hour exposure
+
+      Print (C_exh_ug, 239.9 245 270 360 1320 2760 4260 8580);
+      Data  (C_exh_ug, 340   99  44  33  6.3  3.45 2.1  0.76);
+
+      Print (C_ven, 239.9 360  1320 2760  4260  8580);
+      Data  (C_ven, 2.8   0.92 0.17 0.082 0.055 0.018);
+    }
+
+    Experiment { # 2: Subject A 144 ppm
+
+      LeanBodyWt = 62;
+      Pct_M_fat = 0.114;
+      Flow_pul = 7.6;
+
+      InhMag = 144;   # ppm
+      Period = 1e10;  # Only one dose
+      Exposure = 240; # 4 hour exposure
+
+      Print (C_exh_ug, 239.9 245 270 360 1320 2760 4260 8580);
+      Data  (C_exh_ug, 632   219 116 58  12.9 5.2  3.5  1.2);
+
+      Print (C_ven, 239.9 360  1320 2760  4260  8580);
+      Data  (C_ven, 5.7   1.76 0.36 0.147 0.106 0.072);
+    }
+
+    Experiment { # 3: Subject B 72 ppm
+
+      LeanBodyWt = 71;
+      Pct_M_fat = 0.134;
+      Flow_pul = 11.6;
+
+      InhMag = 72;    # ppm
+      Period = 1e10;  # Only one dose
+      Exposure = 240; # 4 hour exposure
+
+      Print (C_exh_ug, 239.9 245 270 360 1320 2760 4260 8580);
+      Data  (C_exh_ug, 345   101 76  49  6.3  2.7  1.7  0.78);
+
+      Print (C_ven, 239.9 360 1320 2760  4260  8580);
+      Data  (C_ven, 3.0   1.2 0.15 0.066 0.051 0.020);
+    }
+
+    Experiment { # 4: Subject B 144 ppm
+
+      LeanBodyWt = 71;
+      Pct_M_fat = 0.134;
+      Flow_pul = 11.6;
+
+      InhMag = 144;  # ppm
+      Period = 1e10; # Only one dose
+      Exposure = 240; # 4 hour exposure
+
+      Print (C_exh_ug, 239.9 245 270 360 1320 2760 4260 8580);
+      Data  (C_exh_ug, 699   241 120 75  11.4 6.7  4.1  1.3);
+
+      Print (C_ven, 239.9 360 1320 2760 4260 8580);
+      Data  (C_ven, 8.8   2.9 0.36 0.19 0.12 0.036);
+    }
+
+    Experiment { # 5: Subject C 72 ppm
+
+      LeanBodyWt = 71;
+      Pct_M_fat = 0.134;
+      Flow_pul = 10;
+
+      InhMag = 72;    # ppm
+      Period = 1e10;  # Only one dose
+      Exposure = 240; # 4 hour exposure
+
+      Print (C_exh_ug, 239.9 245 270 360 1320 2760 4260 8580);
+      Data  (C_exh_ug, 294   118 83  48  5.2  2.55 1.3  0.65);
+
+      Print (C_ven, 239.9 360  1320  2760  4260  8580);
+      Data  (C_ven, 3.2   1.16 0.115 0.048 0.035 0.015);
+    }
+
+    Experiment { # 6: Subject C 144 ppm
+
+      LeanBodyWt = 71;
+      Pct_M_fat = 0.134;
+      Flow_pul = 10;
+
+      InhMag = 144;  # ppm
+      Period = 1e10; # Only one dose
+      Exposure = 240; # 4 hour exposure
+
+      Print (C_exh_ug, 239.9 245 270 360 1320 2760 4260 5700 10020);
+      Data  (C_exh_ug, 569   178 103 64  11   5.4  3.0  2.0  0.82);
+
+      Print (C_ven, 239.9 360  1320  2760  4260  5700  10020);
+      Data  (C_ven, 6.4   2.36 0.260 0.177 0.085 0.085 0.024);
+    }
+
+    Experiment { # 7: Subject D 72 ppm
+
+      LeanBodyWt = 74;
+      Pct_M_fat = 0.14;
+      Flow_pul = 11.3;
+
+      InhMag = 72;    # ppm
+      Period = 1e10;  # Only one dose
+      Exposure = 240; # 4 hour exposure
+
+      Print (C_exh_ug, 239.9 245 270 360 1320 2760 4260 5700 10020);
+      Data  (C_exh_ug, 329   117 65  30  7.2  2.6  1.62 1.08 0.24);
+
+      Print (C_ven, 239.9 360 1320  2760  4260  5700  10020);
+      Data  (C_ven, 3.1   1.3 0.185 0.068 0.040 0.037 0.0065);
+    }
+
+    Experiment { # 8: Subject D 144 ppm
+
+      LeanBodyWt = 74;
+      Pct_M_fat = 0.14;
+      Flow_pul = 11.3;
+
+      InhMag = 144;  # ppm
+      Period = 1e10; # Only one dose
+      Exposure = 240; # 4 hour exposure
+
+      Print (C_exh_ug, 239.9 245 270 360 1320 2760 4260 5700 10020);
+      Data  (C_exh_ug, 646   249 126 101 11   5.4  2.7  2.1  0.6);
+
+      Print (C_ven, 239.9 360  1320 2760  4260  5700  10020);
+      Data  (C_ven, 6.0   2.48 0.36 0.165 0.071 0.064 0.018);
+    }
+
+    Experiment { # 9: Subject E 72 ppm
+
+      LeanBodyWt = 61;
+      Pct_M_fat = 0.09;
+      Flow_pul = 12.3;
+
+      InhMag = 72;    # ppm
+      Period = 1e10;  # Only one dose
+      Exposure = 240; # 4 hour exposure
+
+      Print (C_exh_ug, 239.9 245 270 360 1320 2760 4260 5700 10020);
+      Data  (C_exh_ug, 360   93  44  21  6.8  2.4  1.4  0.96 0.38);
+
+      Print (C_ven, 239.9 360  1320 2760  4260  5700  10020);
+      Data  (C_ven, 2.8   1.12 0.14 0.068 0.047 0.036 0.014);
+    }
+
+    Experiment { # 10: Subject E 144 ppm
+
+      LeanBodyWt = 61;
+      Pct_M_fat = 0.09;
+      Flow_pul = 12.3;
+
+      InhMag = 144;  # ppm
+      Period = 1e10; # Only one dose
+      Exposure = 240; # 4 hour exposure
+
+      Print (C_exh_ug, 239.9 245 270 360  1320 2760 4260 8580);
+      Data  (C_exh_ug, 686   108 98  65.5 11.2 6.2  3.4  1.4);
+
+      Print (C_ven, 239.9 360  1320 2760 4260  8580);
+      Data  (C_ven, 6.4   2.96 0.35 0.19 0.105 0.05);
+    }
+
+    Experiment { # 11: Subject F 72 ppm
+
+      LeanBodyWt = 61;
+      Pct_M_fat = 0.208;
+      Flow_pul = 8.8;
+
+      InhMag = 72;    # ppm
+      Period = 1e10;  # Only one dose
+      Exposure = 240; # 4 hour exposure
+
+      Print (C_exh_ug, 239.9 245 270 360 1320 2760 4260 5700 10020);
+      Data  (C_exh_ug, 292   64  50  23  4.05 2.5  2.0  1.45 0.9);
+
+      Print (C_ven, 239.9 360  1320  2760  4260  5700  10020);
+      Data  (C_ven, 2.6   0.96 0.105 0.070 0.051 0.050 0.025);
+    }
+
+    Experiment { # 12: Subject F 144 ppm
+
+      LeanBodyWt = 61;
+      Pct_M_fat = 0.208;
+      Flow_pul = 8.8;
+
+      InhMag = 144;  # ppm
+      Period = 1e10; # Only one dose
+      Exposure = 240; # 4 hour exposure
+
+      Print (C_exh_ug, 239.9 245 270 360 1320 2760 4260 5700 10020);
+      Data  (C_exh_ug, 628   193 100 56  9.3  6.0  5.1  3.2  1.5);
+
+      Print (C_ven, 239.9 360  1320  2760 4260 5700  10020);
+      Data  (C_ven, 6.0   1.76 0.245 0.16 0.12 0.098 0.052);
+    }
+  }
+}
+
+End.
