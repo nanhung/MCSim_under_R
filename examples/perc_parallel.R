32c5e2d3bfe79e3fa2b8c943e58fac7095b2bb85
nanhung <nhsieh@cvm.tamu.edu>
2019-06-01 14:27:53 -0500

    add perc parallel

---
+++ examples/perc_parallel.R
@@ -0,0 +1,19 @@
+library(foreach)     
+library(doParallel)  
+
+detectCores()
+cores <- 3
+cl <- makeCluster(3)                   
+registerDoParallel(cl)
+
+# Create executable program
+makemcsim("perc.model.R", dir = "modeling/perc")
+
+# Parallel computing
+strt<-Sys.time()
+system.time( 
+  out <- foreach(i = 1:3) %dopar% { mcsim(model = "perc.model.R", input = "perc_mcmc.in.R", dir = "modeling/perc", parallel = T)  }
+)
+print(Sys.time()-strt)
+
+stopCluster(cl)   
